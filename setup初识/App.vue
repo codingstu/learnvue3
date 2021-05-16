2e7726a82cc04375d4fd3956725f867b9b7f98ca
zofia-ela <jiangdongfeng01@gmail.com>
2021-05-16 22:53:21 +0800

    提交信息

---
+++ setup初识/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>Hello</div>
+  <h2>{{counter}}</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "APP",
+  setup() {
+    const counter = 1;
+    return {
+      counter,
+    };
+  },
+});
+</script>
+
+<style>
+</style>
